663d27d374396610316d1e8f105d4729179b5282
Allen Guo <guosonglin001@gmail.com>
2025-03-22 16:44:20 +0800

    wip

--- ray_placement_group_0.py
+++ ray_placement_group_0.py
@@ -15,17 +15,30 @@ ray.init(
 
 
 @ray.remote(num_cpus=0, num_gpus=2)
-def f():
+def f0():
     # breakpoint()
     print(f"torch.cuda.device_count: {torch.cuda.device_count()}")
     print(f"CUDA_VISIBLE_DEVICES: {os.environ['CUDA_VISIBLE_DEVICES']}")
     return 0
 
 
-pg = ray.util.placement_group([{"CPU": 0, "GPU": 2}])
+@ray.remote(num_cpus=0, num_gpus=2)
+def f1():
+    # breakpoint()
+    print(f"torch.cuda.device_count: {torch.cuda.device_count()}")
+    print(f"CUDA_VISIBLE_DEVICES: {os.environ['CUDA_VISIBLE_DEVICES']}")
+    return 1
 
-ret = ray.get(
-    f.options(
-        scheduling_strategy=PlacementGroupSchedulingStrategy(placement_group=pg)
-    ).remote()
-)
+
+pg = ray.util.placement_group([{"CPU": 0, "GPU": 4}])
+
+r0 = f0.options(
+    scheduling_strategy=PlacementGroupSchedulingStrategy(placement_group=pg)
+).remote()
+
+
+r1 = f1.options(
+    scheduling_strategy=PlacementGroupSchedulingStrategy(placement_group=pg)
+).remote()
+
+print(ray.get([r1, r0]))
